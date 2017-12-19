1a766b9416319323fe3f328cc1d9b7e2c41d4447
w-jerome <jerome.wohlschlegel@gmail.com>
2017-12-19 16:46:16 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# create-dom-element
-[Javascript] Create DOM element
+# Create DOM elements
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+## Usage
+
+`<script src="https://raw.githubusercontent.com/w-jerome/create-dom-element/master/create-dom-element.js"></script>`
+
+```javascript
+var el = CreateDOM({
+  tag: 'div',
+  attr: {
+    id: 'my-div',
+    class: 'is-div',
+  },
+  html: '<strong>is html</strong>',
+  childrens: [document.querySelector('.my-element')],
+  events: {
+    onclick: function() {
+        console.log('clicked !!');
+    }
+  }
+});
+
+document.body.appendChild(el);
+```
---
+++ create-dom-element.js
@@ -0,0 +1,52 @@
+/*=====================================================
+ *   Made with <3 by Jérôme Wohlschlegel
+ =====================================================*/
+
+function CreateDOM(opt) {
+  this.opt = opt || {};
+
+  if (typeof this.opt.tag !== 'string') {
+    return false;
+  }
+  
+  // Create element
+  var el = document.createElement(this.opt.tag);
+  
+  // Set attributes
+  if (typeof this.opt.attr === 'object') {
+
+    for (var key in this.opt.attr) {
+      el.setAttribute(key, this.opt.attr[key]);
+    }
+  }
+
+  // Set html
+  if (typeof this.opt.html !== 'undefined') {
+    el.innerHTML = this.opt.html;
+  }
+
+  // Set childrens
+  if (typeof this.opt.childrens === 'object' && !Array.isArray(this.opt.childrens)) {
+    this.opt.childrens = [this.opt.childrens];
+  }
+
+  if (typeof this.opt.childrens === 'object' && Array.isArray(this.opt.childrens)) {
+
+    for (var i in this.opt.childrens) {
+      
+      if (this.opt.childrens[i] && typeof this.opt.childrens[i].nodeName === 'string') {
+        el.appendChild(this.opt.childrens[i]);
+      }
+    }
+  }
+
+  // Set events
+  if (typeof this.opt.events === 'object') {
+
+    for (var key in this.opt.events) {
+      el[key] = this.opt.events[key];
+    }
+  }
+  
+  return el;
+}
